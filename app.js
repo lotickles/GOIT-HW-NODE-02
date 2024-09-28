2746554bd9fa0ea6b3da1c6d80f44d16dbd050b3
lotickles <148078790+lotickles@users.noreply.github.com>
2024-09-28 14:25:11 +0800

    completed hw2

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+import express from "express";
+import loggere from "morgan";
+import cors from "cors";
+import { router as contactsRouter } from "./routes/api/contactsRouter.js";
 
-const contactsRouter = require('./routes/api/contacts')
+//initialize an express application
+const app = express();
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+app.use(loggere(formatsLogger));
+app.use(cors());
+app.use(express.json());
 
-const app = express()
+// initialize the base path for the contacts router
+//http://localhost:3000/contacts
+app.use("/api/contacts", contactsRouter);
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
-
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/contacts', contactsRouter)
-
+// error handling using res.status()
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
+//server error
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  res.status(500).json({ message: err.message });
+});
 
-module.exports = app
+export { app };
